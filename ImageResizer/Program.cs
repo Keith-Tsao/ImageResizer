35398e3b7a440dc0a77407e5aa6d68f5838988ed
Keith-Tsao <tkfaypkpa@gmail.com>
2019-10-04 12:31:29 +0800

    針對儲存動作優化

--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -9,10 +9,11 @@ namespace ImageResizer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
-            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
+            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output");
+
 
             ImageProcess imageProcess = new ImageProcess();
 
@@ -23,7 +24,20 @@ namespace ImageResizer
             imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
             sw.Stop();
 
-            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+            destinationPath = Path.Combine(Environment.CurrentDirectory, "output_async");
+            imageProcess.Clean(destinationPath);
+
+            Stopwatch swAsync = new Stopwatch();
+            swAsync.Start();
+            await imageProcess.ResizeImagesAsync(sourcePath, destinationPath, 2.0);
+            swAsync.Stop();
+
+            Console.WriteLine($"調整前花費時間: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine($"調整後花費時間: {swAsync.ElapsedMilliseconds} ms");
+
+            Console.WriteLine($"提升: {((sw.ElapsedMilliseconds - swAsync.ElapsedMilliseconds) / (double)sw.ElapsedMilliseconds * 100).ToString("0.00")} %");
+
+            Console.ReadKey();
         }
     }
 }
